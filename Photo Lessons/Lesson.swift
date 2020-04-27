f400d45d7df8c78c2baa380a8a3206eeacd07cd7
Gabe Guerrero <gguerrero11@gmail.com>
2020-04-27 09:22:09 -0600

    create lesson model

---
+++ Photo Lessons/Lesson.swift
@@ -0,0 +1,42 @@
+//
+//  Lesson.swift
+//  Photo Lessons
+//
+//  Created by Gabe Guerrero on 4/27/20.
+//  Copyright © 2020 Gabriel Guerrero. All rights reserved.
+//
+
+import Foundation
+
+struct Lesson {
+    var id: Int?
+    var name: String?
+    var thumbURL: String?
+    var description: String?
+    var videoURL: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id             = "id"
+        case name           = "name"
+        case thumbURL       = "thumbnail"
+        case description    = "description"
+        case videoURL       = "video_link"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        id = try? container.decode(Int.self, forKey: .id)
+        name = try? container.decode(String.self, forKey: .name)
+        thumbURL = try? container.decode(String.self, forKey: .thumbURL)
+        description = try? container.decode(String.self, forKey: .description)
+        videoURL = try? container.decode(String.self, forKey: .videoURL)
+    
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        
+    }
+    
+}
+
