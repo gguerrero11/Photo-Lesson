632ac201e8456ad7206581efba3dde35de0889cb
Gabe Guerrero <gguerrero11@gmail.com>
2020-04-27 09:11:33 -0600

    Initial Commit

---
+++ Photo Lessons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photo Lessons
+//
+//  Created by Gabe Guerrero on 4/27/20.
+//  Copyright © 2020 Gabriel Guerrero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo LessonsTests/Photo_LessonsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Photo_LessonsTests.swift
+//  Photo LessonsTests
+//
+//  Created by Gabe Guerrero on 4/27/20.
+//  Copyright © 2020 Gabriel Guerrero. All rights reserved.
+//
+
+import XCTest
+@testable import Photo_Lessons
+
+class Photo_LessonsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
